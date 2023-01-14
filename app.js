7f8950ee228e14e4e83b77b2044d5e7931e571f5
Eunbin Kang <kub0803@naver.com>
2023-01-15 04:05:49 +0900

    chat

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 
+let room = ["room1", "room2", "room3"];
+
 const app = express();
 
 const server = http.createServer(app);
@@ -16,21 +18,19 @@ const io = socketIo(server, {
 let interval;
 
 io.on("connection", (socket) => {
-  console.log("New client connected");
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(() => getApiAndEmit(socket), 1000);
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-    clearInterval(interval);
+  socket.on("joinRoom", (name) => {
+    console.log(name);
+    io.emit("joinRoom", name);
+  });
+
+  socket.on("leaveRoom", (name) => {
+    io.emit("leaveRoom", name);
+  });
+
+  socket.on("chat-msg", (name, msg) => {
+    console.log(msg, name);
+    io.emit("chat-msg", name, msg); // to(room[a])를 통해 그룹에게만 메세지를 날린다.
   });
 });
-
-const getApiAndEmit = (socket) => {
-  const response = new Date();
-  // Emitting a new message. Will be consumed by the client
-  socket.emit("FromAPI", response);
-};
 
 server.listen(4000, () => console.log(`Listening on port 4000`));
